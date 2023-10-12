4398ac21d081a8be258ab7b63c2d9461f4bc21d0
Michael LoPresti <mdl4071@yum.com>
2023-10-11 23:47:40 -0700

    get build working

--- ValidateJson.build.ps1
+++ ValidateJson.build.ps1
@@ -4,26 +4,31 @@ param(
     $module_name = "ValidateJson"
 )
 $module_name = "ValidateJson"
-$nuspecPath = "$BuildRoot/$module_name/$module_name.nuspec"
+$nuspecPath = "$BuildRoot\src\$module_name.nuspec"
 
 # Synopsis: Remove temp files.
 task clean {
-	remove "$module_name/lib/**", "$module_name/$module_name.nuspec", "$module_name/$module_name.psd1", "*.zip", "dist"
+	remove "src\lib\**", "src\$module_name.nuspec", "src\$module_name.psd1", "*.zip", "dist"
 }
 task version {
-    $template=[xml](Get-Content "$BuildRoot/tools/nuspec.template")
+    $template=[xml](Get-Content "$BuildRoot\tools\nuspec.template")
     $script:Version = $template.package.metadata.version
     Write-Build Green "Version is $script:Version"
 }
 task fetch_required_packages {
-    $script:RequiredPackages = (Import-PowerShellDataFile "$BuildRoot/$module_name/PackageRequirements.psd1")['Packages']
+    $script:RequiredPackages = (Import-PowerShellDataFile "$BuildRoot\src\PackageRequirements.psd1")['Packages']
+}
+task fetch_dll_paths {
+    $script:dllPaths = Get-ChildItem "$BuildRoot\src\" -Recurse -include "*net45*" | `
+        Select-Object -ExpandProperty Fullname | `
+        Where-Object {$_ -notlike "*portable*"}
 }
 
 # install nuget packages
 task install_packages fetch_required_packages, {
     $script:RequiredPackages | foreach-object {
-        if(-not (Test-Path "$BuildRoot/$module_name/lib/$($_['package']).$($_['version'])")) {
-            nuget install  $_['package'] -OutputDirectory "$BuildRoot/$module_name/lib/" -Version $_['version'] | out-null
+        if(-not (Test-Path "$BuildRoot\src\lib\$($_['package']).$($_['version'])")) {
+            nuget install  $_['package'] -OutputDirectory "$BuildRoot\src\lib\" -Version $_['version'] | out-null
             Write-Build Green "Installing $($_['package']).$($_['version'])"
         } else {
             Write-Build Green "$($_['package']).$($_['version']) is already installed, skipping."
@@ -38,7 +43,7 @@ task generate_nuspec fetch_required_packages, {
         $manifest = [xml](Get-Content $nuspecPath)
         Write-Build Green "Updating existing nuspec file"
     } else {
-        $manifest = [xml](Get-Content "$BuildRoot/tools/nuspec.template")
+        $manifest = [xml](Get-Content "$BuildRoot\tools\nuspec.template")
         Write-Build Yellow "Creating new nuspec file from template"
     }
     $manifest.package.metadata.id = $module_name
@@ -50,9 +55,10 @@ task generate_nuspec fetch_required_packages, {
 }
 
 # generate the manifest
-task generate_manifest generate_nuspec, {
+task generate_manifest generate_nuspec, fetch_dll_paths, {
     $manifest = [xml](Get-Content $nuspecPath)
-    New-ModuleManifest -Path "$BuildRoot/$module_name/$module_name.psd1" `
+    New-ModuleManifest -Path "$BuildRoot\src\$module_name.psd1" `
+        -Description $manifest.package.metadata.description `
         -Author "mdlopresti" `
         -FunctionsToExport "Test-Json" `
         -CmdletsToExport "" `
@@ -60,9 +66,12 @@ task generate_manifest generate_nuspec, {
         -Copyright $manifest.package.metadata.copyright `
         -Guid "b8e80ca6-a7fb-4bf3-b9fd-b0f8d99488a2" `
         -ModuleVersion $manifest.package.metadata.version `
-        -RootModule "$BuildRoot/$module_name/$module_name.psm1" `
+        -RootModule "$module_name.psm1" `
         -PowerShellVersion "5.1" `
-        -Tags $manifest.package.metadata.tags
+        -Tags $manifest.package.metadata.tags `
+        -LicenseUri ($manifest.package.metadata.projectUrl + "/blob/main/LICENSE.md") `
+        -ProjectUri $manifest.package.metadata.projectUrl `
+        -FileList ($script:dllPaths | foreach-object { $_.replace("$BuildRoot\src",".")})
 }
 
 # increase version
@@ -77,15 +86,15 @@ task version_bump version, {
     $manifest = [xml](Get-Content $nuspecPath)
     $manifest.package.metadata.version = $script:Version
     $manifest.Save($nuspecPath)
-    $manifestTemplate = [xml](Get-Content "$BuildRoot/tools/nuspec.template")
+    $manifestTemplate = [xml](Get-Content "$BuildRoot\tools\nuspec.template")
     $manifestTemplate.package.metadata.version = $script:Version
-    $manifestTemplate.Save("$BuildRoot/tools/nuspec.template")
+    $manifestTemplate.Save("$BuildRoot\tools\nuspec.template")
 
-    Update-ModuleManifest -Path "$BuildRoot/$module_name/$module_name.psd1" -ModuleVersion $script:Version
+    Update-ModuleManifest -Path "$BuildRoot\src\$module_name.psd1" -ModuleVersion $script:Version
 }
 
 task analyze {
-    $output = Invoke-ScriptAnalyzer "$BuildRoot/$module_name"
+    $output = Invoke-ScriptAnalyzer "$BuildRoot\src\$module_name.psm1"
     if($output){
         $output
         throw "Script Analysis Failed"
@@ -95,15 +104,13 @@ task analyze {
 
 task generate_package generate_manifest, {
     New-Item -Path $BuildRoot -Name "dist" -ItemType "directory" -Force | out-null
-    Copy-Item -Path "$BuildRoot/$module_name/*.psm1","$BuildRoot/$module_name/*.psd1" -Destination "$BuildRoot/dist/"
-    Get-ChildItem "$BuildRoot/$module_name/" -Recurse -include "*net45*" | `
-        Select-Object -ExpandProperty Fullname | `
-        Where-Object {$_ -notlike "*portable*"} | `
-        foreach-object {
-            $fullpath = $_
-            $relativePath = $fullpath.replace("$BuildRoot\$module_name",".")
-            Copy-Item -Path $fullpath -Destination "$BuildRoot\dist\$relativePath" -Recurse
-        }
+    New-Item -Path "$BuildRoot\dist" -Name $module_name -ItemType "directory" -Force | out-null
+    Copy-Item -Path "$BuildRoot\src\*.psm1","$BuildRoot\src\*.psd1" -Destination "$BuildRoot\dist\$module_name\"
+    $script:dllPaths | foreach-object {
+        $fullpath = $_
+        $relativePath = $fullpath.replace("$BuildRoot\src",".")
+        Copy-Item -Path $fullpath -Destination "$BuildRoot\dist\$module_name\$relativePath" -Recurse -Force
+    }
 
 }
 
@@ -112,3 +119,7 @@ task . generate_nuspec, generate_manifest, install_packages, analyze, generate_p
 task rebuild clean, generate_manifest, install_packages
 
 task package rebuild, generate_package, analyze
+
+task publish install_packages, generate_package, {
+    Publish-Module -Path "$BuildRoot\dist\$module_name" -NuGetApiKey $env:NuGetApiKey
+}
