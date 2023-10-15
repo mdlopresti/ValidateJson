7c8940fb432fa0241571681ddfcf0ea838bbb301
Michael LoPresti <mike@lopresti.org>
2023-10-14 23:02:00 -0700

    Fix build (#6)
    Co-authored-by: Michael LoPresti <mdl4071@yum.com>

--- tools/dev-init.ps1
+++ tools/dev-init.ps1
@@ -1,5 +1,8 @@
-Install-Module InvokeBuild,PSScriptAnalyzer,platyPS -Scope CurrentUser -Force
-Install-Module Pester -Scope CurrentUser -MinimumVersion "5.0.0"
+Install-Module "InvokeBuild","PSScriptAnalyzer","platyPS" -Scope CurrentUser -Force
+Install-Module "Pester" -Scope CurrentUser -MinimumVersion "5.0.0" -Force
+
+Write-host "Module Versions are"
+Get-Module -ListAvailable "InvokeBuild","PSScriptAnalyzer","platyPS","Pester" | Select-Object Name,Version
 
 if($(winget list --id Microsoft.NuGet --disable-interactivity --accept-source-agreements|out-string) -like '*No installed package found*') {
     winget install -e --id Microsoft.NuGet --disable-interactivity --accept-source-agreements
